1fea061c1220f91be5de36f12c31c0ce96247e70
Tanaporn8084 <tanaporn8048@gmail.com>
2025-08-06 01:00:05 +0700

    8/6

---
+++ Py Da/new_train/10.py
@@ -0,0 +1,9 @@
+age = int(input("what is your age")) 
+if age > 30:
+    print("you are over 30 year old \nsorry,you do not quality")
+   
+elif age == 30:
+    print("you are 30 \nstill younger")
+  
+else:
+    print("you are 30 year old younger\ncongreatolation")
---
+++ Py Da/new_train/11.py
@@ -0,0 +1,27 @@
+#password = input("do you know the secret password")
+#if password != "-- secret":
+    #print("not correct")
+#else:
+    #print("correct password") 
+
+#age = int(input("what is your age")) 
+#country = input("what is your country?")
+
+#if age < 30 and country == "thai" :
+    #print("you can apply")
+#else:
+    #print("you are not quality")
+
+#age = int(input("what is your age")) 
+#country = input("what is your country?")
+
+#if age < 30 or country == "thai" :
+ #   print("you can apply")
+#else:
+    #print("you are not quality") 
+
+country = input("what is your country?")
+if not country == "thai":
+    print("you are not thai")
+else:
+    print("you are thai")
